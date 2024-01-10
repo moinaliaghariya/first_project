8759da201d16b2700284b90591f452805e5705c1
Moinali <moinaliagharia@gmail.com>
2024-01-10 21:34:19 +0530

    First commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,25 @@
+ # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+
+{
+    'name': 'hospital management',
+    'version': '1.2',
+    'category': '',
+    'summary': '',
+    'description': """
+    """,
+    'depends': [
+
+    ],
+    'data': [
+        ],
+    'demo': [
+        ],
+    'installable': True,
+    'assets': {
+
+    },
+
+}
